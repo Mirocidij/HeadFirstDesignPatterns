dc69fb740c5bd0fddc11a366c976602079168837
Yatsutko Sergey <s3rzh@inbox.ru>
2020-07-04 20:51:25 +0700

    init

---
+++ src/main/java/com/mirocidij/headfirst/patterns/chapter1/Duck.java
@@ -0,0 +1,7 @@
+package main.java.com.mirocidij.headfirst.patterns.chapter1;
+
+public class Duck {
+    public void quack() {}
+    public void swim() {}
+    public void display() {}
+}
---
+++ src/main/java/com/mirocidij/headfirst/patterns/chapter1/MallardDuck.java
@@ -0,0 +1,8 @@
+package main.java.com.mirocidij.headfirst.patterns.chapter1;
+
+public class MallardDuck extends Duck {
+    @Override
+    public void display() {
+
+    }
+}
